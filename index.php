d0ddf791bd0415847b50f0af2acdf2ae7aaa403c
けお <o.kengo1987@gmail.com>
2020-05-21 17:02:15 +0900

    終了

--- index.php
+++ index.php
@@ -8,3 +8,74 @@ function showInfo(string $name, int $score): void
 {
   echo $name . ':' .$score . PHP_EOL;
 }
+//////////////////////////
+// 型
+//////////////////////////
+
+// 型の前に ? マークを付けてあげると、null かその型かという意味になる
+// ただし、現時点ではnullかそれ以外かという指定はできるが、整数か文字列かとか、文字列か真偽値かといった指定はできない
+function getAward(?int $score): ?string
+{
+  return $score >= 100 ? 'Gold Medal' : null;
+}
+
+echo getAward(150) . PHP_EOL;
+echo getAward(40) . PHP_EOL;
+
+
+
+//////////////////////////
+// 配列
+//////////////////////////
+
+$moreScores = [
+  55, 72
+];
+
+// 配列に配列を追加したい場合は...の後に配列を加える
+$scores = [
+  90,
+  40,
+  ...$moreScores,
+  100
+];
+
+print_r($scores);
+
+//////////////////////////
+// 可変長引数
+//////////////////////////
+
+// 引数に...をつけると引数がいくつあっても処理できる
+function sum(...$numbers)
+{
+  $total = 0;
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return $total;
+}
+
+echo sum(2,3,4) . PHP_EOL;
+echo sum(2,3,4,5,5) . PHP_EOL;
+
+
+//////////////////////////
+// 返り値で配列を受け取る
+//////////////////////////
+function getStates(...$numbers)
+{
+  $total = 0;
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return [$total, $total / count($numbers)];
+}
+
+print_r(getStats(2,5,6));
+
+// listは代入した値が引数に入って配列を作る
+list($sum, $average) = getStats(1,3,5);
+[$sum, $average] = getStats(1,3,5);
+
+echo $sum . PHP_EOL;
